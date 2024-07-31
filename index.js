b47011c32234be2df5a7cce702075f01eacb9eda
din-islam-rajin <din.islam.rajin572@gmail.com>
2024-08-01 00:38:25 +0600

    initialize Project

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:3000", ""],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("Mars Visitor");
+    const marsVisitorCollection = db.collection("marsVisitor");
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+// Test route
+app.get("/", (req, res) => {
+  const serverStatus = {
+    message: "Mars Visitor server is running smoothly",
+  };
+  res.json(serverStatus);
+});
